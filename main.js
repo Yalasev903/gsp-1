481d6a043b4ffea56426de4c7be9474adb033b17
Yaroslav <slasev903@gmail.com>
2021-08-02 20:24:28 +0300

    one commit

--- main.js
+++ main.js
@@ -3,11 +3,16 @@
 let box = document.querySelector('#box');
 window.addEventListener('click', function (event) {
     
-     box.style.left = (event.clientX - box.offsetWidth/2) + 'px';
-     box.style.top = (event.clientY - box.offsetHeight/2) + 'px';
-     console.log(event.clientY);
+    if (box.classList.contains('active')) {
+          box.style.left = (event.clientX - box.offsetWidth/2) + 'px';
+          box.style.top = (event.clientY - box.offsetHeight/2) + 'px';
+}
 });
 
+
+window.box.addEventListener('click', function () {
+ this.classList.add('active');
+});
 
 
 
